17ec876d61bd73b9131d7fe22194846cc623d788
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2016-01-17 02:14:16 +0200

    google leaflet

--- www/js/controllers/clinics.js
+++ www/js/controllers/clinics.js
@@ -5,30 +5,6 @@ app.controller('ClinicsCtrl', function($scope, $stateParams) {
 
 .controller('ClinicDetailsCtrl', function ($scope, $rootScope, $cordovaGeolocation, $stateParams, $ionicModal, $http) {
   $scope.map = {
-    defaults: {
-      tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-      maxZoom: 18,
-      zoomControlPosition: 'bottomleft'
-    },
-    layers: {
-        baselayers: {
-            googleTerrain: {
-                name: 'Google Terrain',
-                layerType: 'TERRAIN',
-                type: 'google'
-            },
-            googleHybrid: {
-              name: 'Google Hybrid',
-              layerType: 'HYBRID',
-              type: 'google'
-          },
-            googleRoadmap: {
-                name: 'Google Streets',
-                layerType: 'ROADMAP',
-                type: 'google'
-            }
-        }
-    },
     markers : {},
     center: {
       lat : $rootScope.coords.lat,
@@ -43,61 +19,79 @@ app.controller('ClinicsCtrl', function($scope, $stateParams) {
     }
   };
 
-  // $scope.fechPoints = function () {
-  //   $http({
-  //     method: 'GET',
-  //     url: 'http://testerjson.herokuapp.com/jsons/280.json',
-  //     params: {}
-  //   }).success(function(data) {
-  //     $scope.points = data.city.all_main_points;
+  angular.extend($scope, {
+    layers: {
+        baselayers: {
+            googleTerrain: {
+                name: 'Google Terrain',
+                layerType: 'TERRAIN',
+                type: 'google'
+            },
+            googleHybrid: {
+                name: 'Google Hybrid',
+                layerType: 'HYBRID',
+                type: 'google'
+            },
+            googleRoadmap: {
+                name: 'Google Streets',
+                layerType: 'ROADMAP',
+                type: 'google'
+            }
+        }
+    },
+    defaults: {
+        scrollWheelZoom: false
+    }
+  });
 
+  $scope.setClinics = function () {
+    if ($scope.clinics) {
 
-  //     $scope.map.center  = {
-  //       lat : $scope.points[0].latitude,
-  //       lng : $scope.points[0].longitude,
-  //       zoom : 12
-  //     };
+      $scope.map.center  = {
+        lat : $rootScope.coords.lat,
+        lng : $rootScope.coords.lng,
+        zoom : 12
+      };
 
-  //     angular.forEach($scope.points, function(point, index) {
-  //       $scope.map.markers[point.id] = {
-  //         lat: point.latitude,
-  //         lng: point.longitude,
-  //         message: point.name,
-  //         // focus: true,
+      angular.forEach($scope.clinics, function(clinic, index) {
+        $scope.map.markers[clinic.id] = {
+          lat: clinic.lat,
+          lng: clinic.lng,
+          message: clinic.name,
+          // focus: true,
+          draggable: false,
+          icon: ''
+        };
+      });
+    };
+
+  }
+
+  $scope.setClinics();
+
+  // $scope.locate = function(){
+  //   $cordovaGeolocation
+  //     .getCurrentPosition()
+  //     .then(function (position) {
+  //       $scope.map.center.lat  = position.coords.latitude;
+  //       $scope.map.center.lng = position.coords.longitude;
+  //       $scope.map.center.zoom = 15;
+
+  //       $scope.map.markers.now = {
+  //         lat:position.coords.latitude,
+  //         lng:position.coords.longitude,
+  //         message: "You Are Here",
+  //         focus: true,
   //         draggable: false
   //       };
+
+  //     }, function(err) {
+  //       // error
+  //       console.log("Location error!");
+  //       console.log(err);
   //     });
-  //     console.log(data);
-  //   }).error(function(data) {
-  //     console.log(data);
-  //   });
-  // }
 
-  // $scope.fechPoints();
-
-  $scope.locate = function(){
-    $cordovaGeolocation
-      .getCurrentPosition()
-      .then(function (position) {
-        $scope.map.center.lat  = position.coords.latitude;
-        $scope.map.center.lng = position.coords.longitude;
-        $scope.map.center.zoom = 15;
-
-        $scope.map.markers.now = {
-          lat:position.coords.latitude,
-          lng:position.coords.longitude,
-          message: "You Are Here",
-          focus: true,
-          draggable: false
-        };
-
-      }, function(err) {
-        // error
-        console.log("Location error!");
-        console.log(err);
-      });
-
-  };
-  $scope.locate();
+  // };
+  // $scope.locate();
 })
 ;
