d4f3daaab4255236fc6a7c642dcaf4b10715e967
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2016-01-17 11:36:01 +0200

    plantago hero

--- www/js/controllers/quiz.js
+++ www/js/controllers/quiz.js
@@ -1,4 +1,4 @@
-app.controller('QuizCtrl', function($scope, $stateParams, $state, $ionicSlideBoxDelegate) {
+app.controller('QuizCtrl', function($scope, $stateParams, $state, $ionicSlideBoxDelegate, $rootScope) {
 
   $scope.disableSwipe = function() {
     $ionicSlideBoxDelegate.enableSlide(false);
@@ -16,7 +16,8 @@ app.controller('QuizCtrl', function($scope, $stateParams, $state, $ionicSlideBox
     $state.go('app.clinics');
   }
 
-  $scope.slideTo = function(event, index) {
+  $scope.slideTo = function(event, index, put_on) {
+    $rootScope.put_on = (typeof put_on === 'undefined') ? '' : put_on;
     angular.element(document.getElementsByClassName("quiz-list-answer")).removeClass('active-answer');
     angular.element(event.target).addClass('active-answer');
     $ionicSlideBoxDelegate.slide(index, 1000);
