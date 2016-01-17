b6bdef2d534b578c42d53b51c0d9fb6532c73231
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2016-01-17 03:40:39 +0200

    quiz active tab

--- www/js/controllers/quiz.js
+++ www/js/controllers/quiz.js
@@ -5,19 +5,19 @@ app.controller('QuizCtrl', function($scope, $stateParams, $state, $ionicSlideBox
   };
 
   $scope.go_plantato = function (event) {
-    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(document.getElementsByClassName("quiz-list-answer")).removeClass('active-answer');
     angular.element(event.target).addClass('active-answer');
     $state.go('app.plantago_hero');
   }
 
   $scope.go_clinics = function (event) {
-    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(document.getElementsByClassName("quiz-list-answer")).removeClass('active-answer');
     angular.element(event.target).addClass('active-answer');
     $state.go('app.clinics');
   }
 
   $scope.slideTo = function(event, index) {
-    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(document.getElementsByClassName("quiz-list-answer")).removeClass('active-answer');
     angular.element(event.target).addClass('active-answer');
     $ionicSlideBoxDelegate.slide(index, 1000);
   };
