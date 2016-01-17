7a2556685f84ff7761a27d56a52f0ee7363bd9a0
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2016-01-17 03:30:53 +0200

    quiz styles

--- www/js/controllers/quiz.js
+++ www/js/controllers/quiz.js
@@ -4,16 +4,22 @@ app.controller('QuizCtrl', function($scope, $stateParams, $state, $ionicSlideBox
     $ionicSlideBoxDelegate.enableSlide(false);
   };
 
-  $scope.go_plantato = function () {
+  $scope.go_plantato = function (event) {
+    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(event.target).addClass('active-answer');
     $state.go('app.plantago_hero');
   }
 
-  $scope.go_clinics = function () {
+  $scope.go_clinics = function (event) {
+    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(event.target).addClass('active-answer');
     $state.go('app.clinics');
   }
 
-  $scope.slideTo = function(index) {
-    $ionicSlideBoxDelegate.slide(index, 1000)
+  $scope.slideTo = function(event, index) {
+    // angular.element('.quiz-list-answer').removeClass('active-answer');
+    angular.element(event.target).addClass('active-answer');
+    $ionicSlideBoxDelegate.slide(index, 1000);
   };
 
 })
